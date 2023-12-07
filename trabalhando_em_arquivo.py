9240e8f8bc1c557432c86b14fc61410d528a314e
Katarina <89870229+devkatt@users.noreply.github.com>
2023-12-06 23:37:40 -0300

    Update trabalhando_em_arquivo.py

--- trabalhando_em_arquivo.py
+++ trabalhando_em_arquivo.py
@@ -1,4 +1,4 @@
-def cadastro():
+def cadastrar():
     while True:
         nome = input('Digite o seu nome (ou 0 para encerrar): ')
         
@@ -9,12 +9,40 @@ def cadastro():
         sexo = input('Digite o seu sexo (M ou F): ')
         telefone = input('Digite o seu telefone: ')
 
-        # Salva as informações em um arquivo
         with open('meu_nome.txt', 'a') as file:
             file.write(f'{nome}|{idade}|{sexo}|{telefone}\n')
 
+def ler_dados():
+    with open('meu_nome.txt', 'r') as file:
+        for linha in file:
+            dados = linha.strip().split('|')
+            nome, idade, sexo, telefone = dados
+            print(f'• Nome: {nome}')
+            print(f'• Idade: {idade} anos')
+            print(f'• Sexo: {"Masculino" if sexo == "M" else "Feminino"}')
+            print(f'• Telefone: {telefone}')
+            print()  # Adiciona uma linha em branco para separar as entradas
+
+def busca_usuario_pelo_nome(nome_procurado):
+    usuarios_encontrados = []
+    with open('meu_nome.txt', 'r') as file:
+        for linha in file:
+            dados = linha.strip().split('|')
+            nome_usuario = dados[0]
+            if nome_procurado.lower() in nome_usuario.lower():
+                usuarios_encontrados.append(dados)
+    return usuarios_encontrados
+
 def main():
-    cadastro()
+    cadastrar()
+    ler_dados()
+
+    nome_desejado = input('Digite um nome ou parte dele para buscar: ')
+    usuarios_encontrados = busca_usuario_pelo_nome(nome_desejado)
+    
+    print(f'\nUsuários com nome contendo "{nome_desejado}":')
+    for usuario in usuarios_encontrados:
+        print(usuario)
 
 if __name__ == "__main__":
     main()
